715f54de182f7a80dd68425df7d05e004f4f1cb5
Constantine Sazonov <svedinkvidon@gmail.com>
2024-06-27 21:41:20 +0500

    added prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	const _PORT = 3000;
+
+	var config = new DocumentBuilder()
+		.setTitle("Api Blog-List")
+		.setVersion("1.0")
+		.build();
+
+	var document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document)
+  
+  await app.listen(_PORT, () => {
+		console.log(`Server started on port ${_PORT}`);
+	});
 }
 bootstrap();
